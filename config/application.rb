8c89aa805a807697766a2fd8c685372e736ad246
Luiz Henrique <luizhenbernardes@gmail.com>
2023-08-11 20:43:04 -0300

    rspec install

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,16 @@ module ProjectMonsterL
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Don´t generate system test files
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
   end
 end
